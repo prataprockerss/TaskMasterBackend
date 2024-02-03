684102b701ae8e938289493930c67da6b58b9a67
pratap.greenstory <pratap@greenstory.io>
2024-02-03 21:15:30 +0530

    setup env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  
+  const PORT = config.get('PORT')
+  await app.listen(PORT);
 }
 bootstrap();
