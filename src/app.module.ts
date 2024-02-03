2d35d52bdfa31a96263cfe3d7d1243d7ab37518a
pratap.greenstory <pratap@greenstory.io>
2024-02-03 21:17:26 +0530

    updated for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DbService } from './config/db.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService, DbService],
 })
